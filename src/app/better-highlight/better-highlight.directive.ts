765f098282ae099b9672fff50851f216ef2f5e3b
jsf2184 <jsf2184@gmail.com>
2018-02-28 19:02:57 -0600

    Add property binding to custom directive

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,28 +1,51 @@
-import {Directive, ElementRef, HostListener, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
+  // On the element that this directive is on, bind the style backgroundColor to this string.
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+
+  // We can make the various colors associated with this directive be settable properties.
+  @Input() defaultColor = 'transparent';
+  @Input() mouseDownColor = 'lightblue';
+  @Input() mouseEnterColor = 'yellow';
+
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(): void {
-    this.renderer.setStyle(this.elementRef.nativeElement,
-      'background-color',
-      'lightgreen');
+    this.backgroundColor = this.defaultColor;
+  }
+
+  @HostListener('mouseenter') onMouseEnter(eventData: Event) {
+    // set the style with the bound property
+    console.log('BetterHighlightDirective.onMouseEnter(): ' + eventData);
+    this.backgroundColor = this.mouseEnterColor;
   }
 
   @HostListener('mousedown') onMouseDown(eventData: Event) {
+    console.log('BetterHighlightDirective.onMouseDown(): ' + eventData);
+    // set the style with the renderer
     this.renderer.setStyle(this.elementRef.nativeElement,
       'background-color',
-      'lightblue');
+      this.mouseDownColor);
   }
 
   @HostListener('mouseup') onMouseUp(eventData: Event) {
+    console.log('BetterHighlightDirective.onMouseUp(): ' + eventData);
+    // set the style with the renderer
     this.renderer.setStyle(this.elementRef.nativeElement,
       'background-color',
-      'lightgreen');
+      this.mouseEnterColor);
+  }
+
+
+  @HostListener('mouseleave') onMouseLeave(eventData: Event) {
+    // set the style with the bound property
+    console.log('BetterHighlightDirective.onMouseLeave(): ' + eventData);
+    this.backgroundColor = this.defaultColor;
   }
 
 }
